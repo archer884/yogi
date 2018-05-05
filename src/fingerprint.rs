0109818c677859576b4fe809af19f74b0de195c4
J/A <archer884@gmail.com>
2018-05-05 12:21:27 -0500

    first-cut

---
+++ src/fingerprint.rs
@@ -0,0 +1,72 @@
+use std::io::{self, Read};
+use std::path::Path;
+
+const MAX_SIZE: u64 = 0x800000;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct PartialFingerprint(u64);
+
+impl PartialFingerprint {
+    pub fn from_path<T: AsRef<Path>>(path: T) -> io::Result<Self> {
+        use std::fs::File;
+
+        let file = File::open(path)?;
+        let meta = file.metadata()?;
+        Ok(PartialFingerprint(meta.len()))
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct Fingerprint {
+    length: u64,
+    head: Vec<u8>, // Here we use a vec solely because an array lacks eq and hash definitions.
+    tail: Option<Vec<u8>>,
+}
+
+impl Fingerprint {
+    pub fn from_path<T: AsRef<Path>>(path: T) -> io::Result<Self> {
+        use std::cmp;
+        use std::fs::File;
+        use std::io::Seek;
+        use std::io::SeekFrom;
+
+        let mut file = File::open(path)?;
+        let meta = file.metadata()?;
+
+        let head = hash(&mut file)?;
+        let tail = {
+            if meta.len() > MAX_SIZE {
+                let extra = if MAX_SIZE > meta.len() {
+                    cmp::min(MAX_SIZE, meta.len() - MAX_SIZE)
+                } else {
+                    MAX_SIZE
+                };
+
+                file.seek(SeekFrom::End(extra as i64))?;
+                Some(hash(&mut file)?)
+            } else {
+                None
+            }
+        };
+
+        Ok(Fingerprint {
+            length: meta.len(),
+            head,
+            tail,
+        })
+    }
+}
+
+fn hash(mut stream: impl Read) -> io::Result<Vec<u8>> {
+    use sha2::Sha256;
+    use digest::{Input, FixedOutput};
+
+    // We need box emplacement here because, otherwise, it blows the stack.
+    let mut buf = box [0u8; MAX_SIZE as usize];
+    let len = stream.read(&mut *buf)?;
+
+    let mut hasher = Sha256::default();
+    hasher.process(&buf[..len]);
+
+    Ok(hasher.fixed_result().into_iter().collect())
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#![feature(box_syntax)]
+
+extern crate digest;
+extern crate sha2;
+extern crate walkdir;
+
+mod fingerprint;
+
+use walkdir::DirEntry;
+
+fn main() {
+    use fingerprint::*;
+    use std::collections::HashMap;
+    use std::env;
+    use std::process;
+    use std::cmp::Reverse;
+
+    let root = match env::args().nth(1) {
+        Some(root) => root,
+        None => {
+            eprintln!("Please provide a root directory");
+            process::exit(1);        
+        }
+    };
+
+    let mut files_by_partial_fingerprint = HashMap::new();
+    for file in list_files(&root) {
+        if let Ok(partial) = PartialFingerprint::from_path(file.path()) {
+            files_by_partial_fingerprint
+                .entry(partial)
+                .or_insert_with(Vec::new)
+                .push(file.path().to_owned());
+        }
+    }
+
+    let mut files_by_fingerprint = HashMap::new();
+    for (_, paths) in files_by_partial_fingerprint {
+        if paths.len() < 2 {
+            continue;
+        }
+
+        for path in paths {
+            if let Ok(fingerprint) = Fingerprint::from_path(&path) {
+                files_by_fingerprint
+                    .entry(fingerprint)
+                    .or_insert_with(Vec::new)
+                    .push(path);
+            }
+        }
+    }
+
+    for (_, mut paths) in files_by_fingerprint {
+        if paths.len() > 1 {
+            let mut paths: Vec<_> = paths
+                .into_iter()
+                .map(|path| path.display().to_string())
+                .collect();
+            
+            // We reverse this comparison because it is desirable to retain files with 
+            // more descriptive names rather than files with less descriptive names.
+            // Descriptive names are usually longer.
+            paths.sort_by_key(|path| Reverse(path.len()));
+
+            for path in paths.into_iter().skip(1) {
+                println!("{}", path);
+            }
+        }
+    }
+}
+
+fn list_files(root: &str) -> impl Iterator<Item = DirEntry> {
+    use walkdir::WalkDir;
+    WalkDir::new(root).into_iter()
+        .filter_map(Result::ok)
+        .filter(|entry| entry.file_type().is_file())
+}
