f54a59fa33f954422d227ff08f520c6336679c3a
J/A <archer884@gmail.com>
2020-06-17 22:18:57 -0500

    Correct hex formatter

--- src/format.rs
+++ src/format.rs
@@ -34,6 +34,6 @@ pub struct HexFormatter<'a>(pub &'a [u8]);
 
 impl LowerHex for HexFormatter<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.iter().try_for_each(|&u| write!(f, "{:x}", u))
+        self.0.iter().try_for_each(|&u| write!(f, "{:02x}", u))
     }
 }
